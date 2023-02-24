8fcad5ac7f42f8a6d7a350fae3ef75a261617894
Realf <stefan.hodgman@gmail.com>
2023-02-24 04:40:10 +0200

    Produces correct output but need to separate json files for ease of use

---
+++ Uniclass2JSONListBuilder.py
@@ -0,0 +1,93 @@
+import os
+import pandas as pd
+import json
+import tkinter as tk
+from tkinter import filedialog
+import traceback
+
+# Create a Tkinter root window to open a file dialog and select a directory
+root = tk.Tk()
+root.withdraw()
+
+# Open a file dialog to select a directory
+directory = filedialog.askdirectory(title="Select a directory")
+
+# Create a list to store the processed codes dictionaries
+all_codes_dict = {}
+
+# Create a list to store the filenames of files that failed to process
+failed_files = []
+
+# Create a variable to keep track of the user's choice to skip lines for all files
+skip_lines_all = False
+
+# Iterate over each file in the directory
+for filename in os.listdir(directory):
+    # Check if the file is an Excel file
+    if filename.endswith(".xlsx") or filename.endswith(".xls"):
+        filepath = os.path.join(directory, filename)
+
+        try:
+            # Ask user if they want to skip the first two lines
+            if not skip_lines_all:
+                skip_lines = tk.messagebox.askyesnocancel("Skip Lines", f"Skip first two lines of {filename}?")
+
+                if skip_lines is None:  # user clicked Cancel
+                    break
+                elif skip_lines:  # user clicked Yes
+                    skip_lines_all = tk.messagebox.askyesno("Skip Lines", "Skip first two lines for all files?")
+            else:
+                skip_lines = True
+
+            # Load Excel file into a pandas dataframe
+            if skip_lines:
+                df = pd.read_excel(filepath, sheet_name=0, usecols=["Code", "Title"], skiprows=2)
+            else:
+                df = pd.read_excel(filepath, sheet_name=0, usecols=["Code", "Title"])
+
+            # Create an empty dictionary to store the codes
+            codes_dict = {}
+
+            # Iterate over each row in the dataframe
+            for index, row in df.iterrows():
+                code = row["Code"]
+                default_name = row["Title"]
+                value = code
+
+                # Assume that the parent code is all but the last two characters of the code
+                parent_code = code[:-2]
+
+                # Remove any trailing underscores from the parent code
+                parent_code = parent_code.rstrip("_")
+
+                # If the parent code is not in the dictionary, add it as a key with an empty list as the value
+                if parent_code not in codes_dict:
+                    codes_dict[parent_code] = {"defaultName": default_name, "value": parent_code, "children": []}
+
+                # Add the code to the list of child codes for the parent code
+                codes_dict[parent_code]["children"].append({"defaultName": default_name, "value": value})
+
+            # Add the processed codes dictionary to the list of all codes dictionaries
+            all_codes_dict.update(codes_dict)
+
+            # Save the processed codes dictionary to a JSON file with the same name as the input file
+            output_filename = os.path.splitext(filename)[0] + "_List.json"
+            output_filepath = os.path.join(directory, output_filename)
+            with open(output_filepath, "w") as json_file:
+                json.dump(list(codes_dict.values()), json_file, separators=(',', ':'), indent=2)
+
+        except Exception as e:
+            # If there was an error processing the file, add it to the list of failed files
+            failed_files.append(filename)
+            traceback.print_exc()
+            continue
+
+# If there were failed files, show a message box with the list of failed files
+if failed_files:
+    tk.messagebox.showwarning("Failed Files", f"The following files failed to process: {', '.join(failed_files)}")
+
+# Convert the merged codes dictionary into a list and save it to a JSON file in the directory
+merged_codes_list = list(all_codes_dict.values())
+output_filepath = os.path.join(directory, "all_codes.json")
+with open(output_filepath, "w") as json_file:
+    json.dump(merged_codes_list, json_file, separators=(',', ':'), indent=2)
